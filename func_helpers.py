e2b2d80140f8d1e1d8cafd0c3452039dc223f3e9
Ivan Abraham <itabrah2@illinois.edu>
2021-04-04 11:09:17 -0500

    Add some download helper functions.

--- func_helpers.py
+++ func_helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import requests
 
 
 def gaussian(x, mu, b=0, k=1):
@@ -60,3 +61,64 @@ def flatten(regular_list):
         A singly nested list to flatten.
     """
     return [item for sublist in regular_list for item in sublist]
+
+
+def download_file_from_google_drive(id, destination):
+    """ Download a drive from Google Drive give id from shareable link. 
+    
+    Parameters
+    ----------
+    id:
+        The file identifier from a shareable link
+    destination:
+        The filename to save as on local disk
+    """
+    
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    """ Function to filter out some Cookie business from Google and
+        extract the actual data
+    
+    Parameters
+    ----------
+    response:
+        The return value from a requests GET request
+    """
+    
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    """ Function to open write the proper response content from a 
+        requests GET response to local disk.
+        
+    Parameters
+    ----------
+    response:
+        The filtered return value from a requests GET request
+    destination:
+        A filename or file object denoting where to save file on
+        local disk
+    """
+    
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
