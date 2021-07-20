229fcc55e2ba74482a8d6e585f7e0c51e50572e4
Ivan Abraham <ivan.abrms@gmail.com>
2021-07-20 13:09:58 -0500

    Change winding count algorithm

--- automaton.py
+++ automaton.py
@@ -1,6 +1,8 @@
 from collections import deque
 
+
 class Automaton:
+    """Class to sort of represent a tape reader/Turing machine."""
 
     def __init__(self, alphabets, start, cw):
         if len(set(alphabets)) < len(alphabets):
@@ -12,35 +14,34 @@ class Automaton:
             while pot_cw[0] != start:
                 pot_cw.rotate(1)
             self.cw = "".join(pot_cw)
-            for x in ["prev_state", "state", "path"]:
+            for x in ["prev_state", "state", "path", "entry", "exit"]:
                 setattr(self, x, None)
 
     def __repr__(self):
+        """Represent automaton's current state."""
         if self.path is None:
             return "Automaton(unset)"
         else:
             return "Automaton({p},{c})".format(p=self.prev_state, c=self.state)
 
     def setPath(self, path):
+        """Set the path/tape for the machine to read."""
         if isinstance(path, str):
             path = list(path)
         elif isinstance(path, list):
             pass
         else:
-            raise TypeError("Path must be a string representing a path or a list\
-                    of alphabets representing a path")
-        
+            raise TypeError("Path must be a string representing a path or a\
+                            list of alphabets representing a path")
+
         if not all(x in self.alphabets for x in path):
             raise LookupError("Path contains non-alphabet characters")
-        if path[0] != self.start:
-            raise IndexError("Path does not start at expected source node")
-        if path[0] != path[-1]:
-            raise TypeError("Path is not closed")
 
         *self.path, = reversed(path)
         return self
 
     def __step(self):
+        """Read one item in the tape, take one step on the path."""
         if self.path is None:
             raise ValueError("No path set to step")
         else:
@@ -49,28 +50,37 @@ class Automaton:
 
         return self
 
+    def __reset_ee(self):
+        self.entry, self.exit = None, None
+
+    def _get_ee(self):
+        return (self.exit, self.entry)
+
     def walkPath(self):
-        winding_no = list()
-        visited = set()
+        """Read the tape a.k.a walk the path."""
+        winding_no, walking = [0], False
+        plusminus = {(self.cw[1], self.cw[-1]): -1,
+                     (self.cw[-1], self.cw[1]): +1}
+
         if self.path is None:
             raise ValueError("No path set to walk")
         else:
-            winding_no.append(0)
             while self.path != []:
                 _ = self.__step()
-                if len(visited)>1 and self.state == self.start:
-                    if self.prev_state == self.cw[-1]:
-                        winding_no.append(winding_no[-1]-1)
-                        visited = set()
-                    elif self.prev_state == self.cw[1]:
-                        winding_no.append(winding_no[-1]+1)
-                        visited = set()
-                    else:
-                        winding_no.append(winding_no[-1])
+                # Either left homebase, entered homebase or still walking
+                if self.prev_state == self.start and self.state != self.start:
+                    self.exit, walking = self.state, True
+                elif walking and self.state == self.start:
+                    self.entry, walking = self.prev_state, False
                 else:
-                    visited.add(self.state)
-                    winding_no.append(winding_no[-1])
+                    curr_winding = winding_no[-1]
+
+                try:  # If at homebase
+                    curr_winding = winding_no[-1] + plusminus[self._get_ee()]
+                    self.__reset_ee()
+                except KeyError:  # Not at homebase
+                    pass
+
+                winding_no.append(curr_winding)
 
         return winding_no
-
-
